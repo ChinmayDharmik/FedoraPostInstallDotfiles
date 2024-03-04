5f9857c5239d8809ff1f1d1c746de1a96000bc9f
Chinmay Dharmik <chinmaydharmik@gmail.com>
2024-03-04 16:09:02 +1030

    fdgg

--- scripts/autoInstallMyApps.sh
+++ scripts/autoInstallMyApps.sh
@@ -4,6 +4,10 @@ curl -sS https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.
 sudo dnf install google-chrome.rpm -y
 rm google-chrome.rpm
 
+curl -sS https://releases.warp.dev/stable/v0.2024.02.27.08.01.stable_03/warp-terminal-v0.2024.02.27.08.01.stable_03-1.x86_64.rpm -o warp.rpm
+sudo dnf install warp.rpm -y
+rm warp.rpm
+
 sudo dnf install vlc htop neofetch kitty htop bpytop -y
 flatpak install flathub com.slack.Slack -y
 flatpak install flathub md.obsidian.Obsidian -y
