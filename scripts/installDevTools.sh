80da6a9504e493186671a9393189e1f5c86f99ca
Chinmay Dharmik <94623147+ChinmayDharmik@users.noreply.github.com>
2024-07-16 11:45:55 +1000

    Update installDevTools.sh

--- scripts/installDevTools.sh
+++ scripts/installDevTools.sh
@@ -4,12 +4,6 @@ sudo dnf install -y git
 ## Miniconda
 curl -sS https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
 bash ./miniconda.sh 
-reset
-conda create -n proj_llm python=3.10 numpy scipy matplotlib pandas jupyterlab ipykernel nltk scikit-learn pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y 
-conda activate proj_llm
-#
-conda deactivate
-conda create -n proj_CV python=3.10 numpy scipy matplotlib pandas jupyterlab ipykernel conda pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 rm miniconda.sh
 ## VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
