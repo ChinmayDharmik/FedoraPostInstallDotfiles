5463f8d9a07f76fe0603f87a51037cad700a7cf8
Chinmay Dharmik <94623147+ChinmayDharmik@users.noreply.github.com>
2024-10-16 22:41:05 +1030

    Update optimizeBootTime.sh

--- scripts/optimizeBootTime.sh
+++ scripts/optimizeBootTime.sh
@@ -1,14 +1,4 @@
 #! /bin/bash
-# ./scripts/optimizeBootTime.sh
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-# fedora-post-install-script
-# Copyright (C) 2022 davidhoang05022009(Hoàng Minh Thiên)
-# This program comes with ABSOLUTELY NO WARRANTY
-# This is free software, and you are welcome to redistribute it
-# under certain conditions
-#
-# Licensed under GPLv3 License
 
 echo "Optimizing boot time for your PC/laptop"
 echo "Checking if your CPU is Intel's CPU or not"
